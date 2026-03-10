updating analyses; fixed PCA weird rotation on baseline/stillwater data

src/PCA.py
@@ -18,9 +18,7 @@ def compute_PCA_summary(data,
     
     results = []
 
-    data_current = data.loc[data[water_col] != 0.0]
-
-    data_group = data_current.groupby(group_cols)
+    data_group = data.groupby(group_cols)
     # now we can group by each unique group and create a dataframe for each thanks to pandas
     for group_name, group_data in data_group:
         
@@ -37,20 +35,15 @@ def compute_PCA_summary(data,
         pca.fit(X_subset)
 
         for i in range(n_components):
-
             pc_vector = pca.components_[i]
 
             pc_id = f"pc_vector_{i+1}"
-
             pc_x = pc_vector[0]
             pc_z = pc_vector[1]
 
-            if pc_z < 0:
-                pc_x, pc_z = -pc_x, -pc_z
-
             pc_deg = np.rad2deg(np.arctan2(pc_z, pc_x))
             pc_rad = np.arctan2(pc_z, pc_x)
-
+            
             # store data
             current_group_row[f'{pc_id}_angle'] = pc_deg
             current_group_row[f'{pc_id}_pca_center_x'] = pca.mean_[0]
@@ -76,37 +69,30 @@ def compute_PCA_error(trial_df,
                    water_col
                      ):
 
-    
-    # remove any still-water trials
-    data_current = trial_df.loc[trial_df[water_col] != 0.0].copy()
-    
     # convert PC vector components into radians, making PC rad col
-    data_current['pc_rad'] = np.arctan2(data_current['pc_vector_1_c_Z'], data_current['pc_vector_1_c_X'])
+    trial_df['pc_rad'] = np.arctan2(trial_df['pc_vector_1_c_Z'], trial_df['pc_vector_1_c_X'])
 
     # Create cos and sin now for when we unrotate the data later through vectorization
-    cos_t = np.cos(-data_current['pc_rad'])
-    sin_t = np.sin(-data_current['pc_rad'])
+    cos_t = np.cos(-trial_df['pc_rad'])
+    sin_t = np.sin(-trial_df['pc_rad'])
 
     # ensure x-axis is the first feature
-    data_current['centered_x'] = data_current[features[0]] - data_current['pc_vector_1_pca_center_x']
-    data_current['centered_z'] = data_current[features[1]] - data_current['pc_vector_1_pca_center_z'] 
+    trial_df['centered_x'] = trial_df[features[0]] - trial_df['pc_vector_1_pca_center_x']
+    trial_df['centered_z'] = trial_df[features[1]] - trial_df['pc_vector_1_pca_center_z'] 
     # unrotate the data using sin and cos on the centred data
-    data_current['unrotated_x'] = (data_current['centered_x'] * cos_t) - (data_current['centered_z'] * sin_t)
-    data_current['unrotated_z'] = (data_current['centered_x'] * sin_t) + (data_current['centered_z'] * cos_t)
+    trial_df['unrotated_x'] = (trial_df['centered_x'] * cos_t) - (trial_df['centered_z'] * sin_t)
+    trial_df['unrotated_z'] = (trial_df['centered_x'] * sin_t) + (trial_df['centered_z'] * cos_t)
     # apply same unrotation to targets
-    data_current['tgt_centered_x'] = data_current[target_x_col] - data_current['pc_vector_1_pca_center_x']
-    data_current['tgt_centered_z'] = data_current[target_z_col] - data_current['pc_vector_1_pca_center_z'] 
+    trial_df['tgt_centered_x'] = trial_df[target_x_col] - trial_df['pc_vector_1_pca_center_x']
+    trial_df['tgt_centered_z'] = trial_df[target_z_col] - trial_df['pc_vector_1_pca_center_z'] 
 
-    data_current['tgt_unrotated_x'] = (data_current['tgt_centered_x'] * cos_t) - (data_current['tgt_centered_z'] * sin_t)
-    data_current['tgt_unrotated_z'] = (data_current['tgt_centered_x'] * sin_t) + (data_current['tgt_centered_z'] * cos_t)
+    trial_df['tgt_unrotated_x'] = (trial_df['tgt_centered_x'] * cos_t) - (trial_df['tgt_centered_z'] * sin_t)
+    trial_df['tgt_unrotated_z'] = (trial_df['tgt_centered_x'] * sin_t) + (trial_df['tgt_centered_z'] * cos_t)
     # x and z errors
-    data_current['PCA_error_X_cm'] = (data_current['unrotated_x'] - data_current['tgt_unrotated_x']) * -1
-    data_current['PCA_error_Z_cm'] = (data_current['unrotated_z'] - data_current['tgt_unrotated_z']) 
+    trial_df['PCA_error_X_cm'] = (trial_df['unrotated_x'] - trial_df['tgt_unrotated_x']) #* -1
+    trial_df['PCA_error_Z_cm'] = (trial_df['unrotated_z'] - trial_df['tgt_unrotated_z']) 
 
-    obtuse_mask = np.abs(data_current['pc_rad']) > (np.pi / 2)
-    data_current.loc[obtuse_mask, 'PCA_error_Z_cm'] *= -1
-
-    return data_current
+    return trial_df
        
 
 